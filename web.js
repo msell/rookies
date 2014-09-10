ccb064e03049dd2e9e46a44036de3a5c0644060b
Matt Sell <mwsell@gmail.com>
2014-09-09 21:55:13 -0500

    remove logger middleware

--- web.js
+++ web.js
@@ -1,6 +1,4 @@
 var express = require('express');
 var app = express();
 
-app.use(express.logger('dev'));
-//app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
