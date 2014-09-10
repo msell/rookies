e388d98104e25bea8b0e8bff419e17f0cd92859e
Matt Sell <mwsell@gmail.com>
2014-09-09 22:02:40 -0500

    im not very good at this

--- web.js
+++ web.js
@@ -1,4 +1,14 @@
-var express = require('express');
+var express = require("express");
+var logfmt = require("logfmt");
 var app = express();
 
-app.listen(process.env.PORT || 5000);
+app.use(logfmt.requestLogger());
+
+app.use(express.static(__dirname + '/app'));
+//add this so the browser can GET the bower files
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+    console.log("Listening on " + port);
+});
